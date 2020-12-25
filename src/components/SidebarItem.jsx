76ef1bd91046c6ff58d0f3cffb137d61b8aa864d
shubham_97 <shubham.agarwal@clix.capital>
2020-12-25 19:32:36 +0530

    no need of isOver

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -3,11 +3,10 @@ import { useDrop } from "react-dnd";
 import { accepatbleMailtypes } from "../constants/index";
 
 export default function SidebarItem({ children, handleDrop, type }) {
-  const [{ isOver, canDrop }, drop] = useDrop({
+  const [{ canDrop }, drop] = useDrop({
     accept: accepatbleMailtypes[type],
     drop: (item) => handleDrop(item, type),
     collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
       canDrop: monitor.canDrop(),
     }),
   });
